034ec8b9fbdc3c7cf9647aef8d0cdd98c6b7d845
Rajeeb Bhowmick <rajeeb1206@gmail.com>
2024-07-31 23:55:31 +0530

    update config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['react', 'react-dom/client'], // Add other external dependencies as needed
+    },
+  },
 })
