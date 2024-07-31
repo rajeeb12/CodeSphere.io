83fbf3067e675434a986e3bf74dc3f333f70b5f7
Rajeeb Bhowmick <rajeeb1206@gmail.com>
2024-08-01 00:51:00 +0530

    upDATE

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['react', 'react-dom/client'], // Add other external dependencies as needed
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
     },
   },
-})
+  // build: {
+  //   rollupOptions: {
+  //     external: ['react', 'react-dom/client'], // Add other external dependencies as needed
+  //   },
+  // },
+});
